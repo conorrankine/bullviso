810b9c3dad2a75ca5516075b0ed22a76ff183c14
Conor Rankine <conor.rankine@york.ac.uk>
2025-02-06 19:32:54 +0000

    committed the `BVBarcode` class to `barcodes.py`

---
+++ bullviso/barcodes.py
@@ -0,0 +1,60 @@
+"""
+BULLVISO
+Copyright (C) 2024  Conor D. Rankine
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either Version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+###############################################################################
+############################### LIBRARY IMPORTS ###############################
+###############################################################################
+
+###############################################################################
+################################### CLASSES ###################################
+###############################################################################
+
+class BVBarcode:
+
+    def __init__(self, barcode: tuple):
+        """
+        Instantiates a bullvalene isomer barcode.
+
+        Args:
+            barcode (tuple): A tuple of 10 integers in the range 0-9 inclusive
+                representing the bullvalene isomer barcode.
+
+        Raises:
+            ValueError: If the barcode i) is not of length 10, ii) contains
+                non-integer elements, or iii) contains integers less than 0
+                or greater than 9 (i.e. not in the range 0-9 inclusive).
+        """
+
+        if len(barcode) == 10 and all(
+            isinstance(x, int) and 0 <= x <= 9 for x in barcode
+        ):        
+            self._barcode = barcode
+        else:
+            raise ValueError(
+                f'{barcode} is not a valid bullvalene isomer barcode'
+            )
+
+    @property
+    def barcode(self) -> tuple:
+        """
+        Returns the bullvalene isomer barcode as a tuple.
+
+        Returns:
+            tuple: The bullvalene isomer barcode as a tuple.
+        """
+
+        return self._barcode
