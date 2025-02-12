1a14f1a0d075d64a4c260b3a6743ce66ca4c7270
Conor Rankine <conor.rankine@york.ac.uk>
2025-02-12 16:47:05 +0000

    deprecated the `functionalise()` function in `geoms.py`

--- bullviso/geoms.py
+++ bullviso/geoms.py
@@ -21,7 +21,6 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import copy
 from rdkit import Chem
-from rdkit.Chem import AllChem
 from rdkit.Chem import rdDistGeom
 from rdkit.Chem import rdForceFieldHelpers
 from rdkit.Chem import rdMolDescriptors
@@ -29,69 +28,6 @@ from rdkit.Chem import rdMolDescriptors
 ###############################################################################
 ################################## FUNCTIONS ##################################
 ###############################################################################
-
-def functionalise(
-    mol: Chem.Mol,
-    func_group: Chem.Mol,
-    confcode: tuple,
-    func_group_attach_idx: int = 1
-) -> Chem.Mol:
-    """
-    Functionalises a Chem.Mol molecule `mol` with the Chem.Mol functional group
-    `func_group` according to a configuration code tuple `confcode`; N 
-    (0 <= N <= `mol.GetNumAtoms()`) instances of `func_group` are attached to
-    `mol` via single bonds at the position(s) indicated in `confcode`.
-
-    Args:
-        mol (Chem.Mol): A molecule to functionalise.
-        func_group (Chem.Mol): A functional group.
-        confcode (tuple): A configuration code encoding the functionalisation
-            of `mol` as a tuple of binary (eg. 1/0) elements with length
-            `mol.GetNumAtoms()`; each binary element indicates whether
-            or not that (functionalisable) site in `mol` is functionalised.
-        func_group_attach_idx (int): An attachment index indicating the
-            position on the functional group `func_group` to be attached to
-            `mol` via single bonds at the position(s) indicated in `confcode`.
-            The atomic numbering scheme starts at 1. Defaults to 1.
-
-    Returns:
-        Chem.Mol: A functionalised molecule.
-    """
-
-    if len(confcode) != mol.GetNumAtoms():
-        raise ValueError('the configuration code `confcode` is incompatible '\
-            'with the molecule `mol`')
-    
-    if func_group_attach_idx > func_group.GetNumAtoms():
-        raise ValueError('the attachment index `func_group_attach_idx` is '\
-            'incompatible with the functional group `func_group`'
-        )
-
-    func_sites_i = tuple(
-        i for i in range(len(confcode)) if confcode[i]
-    )
-
-    func_sites_j = tuple(
-        mol.GetNumAtoms() + (i * func_group.GetNumAtoms()) + (
-            func_group_attach_idx - 1) for i in range(len(func_sites_i))
-    )
-
-    func_mol = Chem.Mol(mol)
-    
-    for func_site_i, func_site_j in zip(func_sites_i, func_sites_j):
-        func_mol = Chem.EditableMol(
-            Chem.CombineMols(func_mol, func_group)
-        )
-        func_mol.AddBond(
-            func_site_i, func_site_j, order = Chem.rdchem.BondType.SINGLE
-        )
-        func_mol = func_mol.GetMol()
-
-    Chem.SanitizeMol(func_mol)
-
-    func_mol = Chem.AddHs(func_mol)
-
-    return func_mol
 
 def generate_confs(
         mol: Chem.Mol,
