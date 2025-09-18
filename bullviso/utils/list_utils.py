4b55ef342285ce0227c137b931af1c655fc22af6
Conor Rankine <206134505+conorrankine@users.noreply.github.com>
2025-09-18 22:21:50 +0100

    deprecated the unusued `count_list_elements()`, `repeat_list_elements()`, `unique_elements()`, and `iterate_and_index()` functions in `utils/list_utils.py`

--- bullviso/utils/list_utils.py
+++ bullviso/utils/list_utils.py
@@ -40,85 +40,6 @@ def all_same_length(
     else:
         return len({len(input_list) for input_list in input_lists}) == 1
 
-def count_list_elements(
-    input_list: list[Any]
-) -> int:
-    """
-    Counts the number of elements in a list, including the elements of any
-    nested (sub)lists recursively.
-
-    Args:
-        input_list (list): Input list.
-
-    Returns:
-        int: Number of elements in the input list.
-    """
-    
-    count = 0
-
-    for i in input_list:
-        if isinstance(i, list):
-            count += count_list_elements(i)
-        else:
-            count += 1
-
-    return count
-
-def repeat_list_elements(
-    input_list: list[Any],
-    repeats: Union[int, list[int]]
-) -> list[Any]:
-    """
-    Repeats elements in a list.
-
-    Args:
-        input_list (list): Input list.
-        repeats (Union[int, list[int]]): Integer or list of integers indicating
-            the number of times that each (corresponding) element in the input
-            list is to be repeated.
-
-    Raises:
-        ValueError: If `repeats` is a list and is not the same length as
-            `input_list`, or if any of the elements of `repeats` are integers
-            less than or equal to zero.
-
-    Returns:
-        _type_: List containing the elements of the input list repeated.
-    """
-    
-    if isinstance(repeats, int):
-        repeats = [repeats] * len(input_list)
-    elif len(repeats) != len(input_list):
-        raise ValueError(
-            '`repeats` and `input_list` should have the same length'
-        )
-
-    if any(repeat <= 0 for repeat in repeats):
-        raise ValueError(
-            '`repeats` cannot contain integers less than or equal to zero'
-        )
-    
-    input_list_repeated = []
-    for i, repeat in zip(input_list, repeats):
-        input_list_repeated.extend([i] * repeat)
-
-    return input_list_repeated
-
-def unique_elements(
-    input_list: list[Any]
-) -> list[Any]:
-    """
-    Returns the unique elements in a list, preserving their order of occurance.
-
-    Args:
-        input_list (list[Any]): Input list.
-
-    Returns:
-        list[Any]: List containing the unique elements of the input list.
-    """
-    
-    return [item for item in dict.fromkeys(input_list)]
-
 def pad_list(
     input_list: list[Any],
     length: int,
@@ -155,40 +76,6 @@ def pad_list(
             )
     else:
         return input_list
-
-def iterate_and_index(
-    input_list: list[Union[Any, list[Any]]]
-) -> Generator[tuple[int, Any], None, None]:
-    """
-    Iterates through a list that contains items and/or nested (sub)lists of
-    items, yielding tuples comprising the index of each item in the outer list
-    and either i) the item, or ii) each of the items in the nested (sub)list.
-
-    Args:
-        input_list (list[Union[Any, list[Any]]]): Input list.
-
-    Raises:
-        ValueError: If `input_list` has a maximum depth greater than 2, i.e.
-            if the items in any nested (sub)list in `input_list` are
-            themselves nested (sub)lists.
-
-    Yields:
-        Generator[tuple[int, Any], None, None]: Generator that yields tuples
-            comprising the index of each item in the outer list and either i)
-            the item, or ii) each of the items in the nested (sub)list.
-    """
-    
-    if maxdepth(input_list) > 2:
-        raise ValueError(
-            '`input_list` cannot have a maximum depth greater than 2'
-        )
-
-    for idx, item in enumerate(input_list):
-        if isinstance(item, list):
-            for item_ in item:
-                yield idx, item_
-        else:
-            yield idx, item
 
 def maxdepth(
     input_list: Any
